2761050e5c2dad01b339ace837b56bc5afa1bc50
Geovane Ferreira <geovane.paixao@ba.estudante.senai.br>
2026-04-01 21:02:31 -0300

    integrando ao MYSQL 20260401

---
+++ api/src/main/java/com/example/api/controller/FornecedorController.java
@@ -0,0 +1,32 @@
+package com.example.api.controller;
+
+
+import com.example.api.model.FornecedorModel;
+import com.example.api.model.FuncionarioModel;
+import com.example.api.repository.FornecedorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/fornecedores")
+public class FornecedorController {
+    @Autowired
+    private FornecedorRepository repository;
+
+    @GetMapping
+    public List<FornecedorModel> listarFornecedores(){
+        return repository.findAll();
+
+    }
+
+    @PostMapping
+    public ResponseEntity<FornecedorModel> salvar (@RequestBody FornecedorModel fornecedores){
+        repository.save(fornecedores);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(fornecedores);
+    }
+}
