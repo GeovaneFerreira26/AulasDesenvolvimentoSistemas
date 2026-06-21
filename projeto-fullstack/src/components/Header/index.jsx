9b065b550884015483c6f2b11cabda911b227c03
Geovane Ferreira <geovane.paixao@ba.estudante.senai.br>
2026-06-21 20:18:32 -0300

    commit com novas atualizações.

--- projeto-fullstack/src/components/Header/index.jsx
+++ projeto-fullstack/src/components/Header/index.jsx
@@ -4,13 +4,13 @@ import './style.css'
 export default function Header(){
     return(
         <header className='header'>
-            <h1>Sistema Gerenciamento de Usuários</h1>
+        <h1>👥 Sistema de Gerenciamento de Usuários</h1>
 
-            <nav>
-                <Link to='/'>Início</Link>
-                <Link to='/cadastro'>Cadastrar</Link>
-                <Link to='/lista-usuarios'>Lista de Usuários</Link>
-            </nav>
+        <nav>
+            <Link to='/'>🏠 Início</Link>
+            <Link to='/cadastro'>➕ Cadastrar</Link>
+            <Link to='/lista-usuarios'>📋 Usuários</Link>
+        </nav>
         </header>
     )
 }
